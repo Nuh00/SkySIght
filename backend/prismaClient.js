3711b305ca394409acb1ed44d492768f1bbf69b9
nuh <noahbarkatwork@gmail.com>
2024-09-25 14:20:56 -0500

    added docker file

--- backend/prismaClient.js
+++ backend/prismaClient.js
@@ -1,11 +1,10 @@
 // Import the PrismaClient from the @prisma/client package
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 
 // Instantiate a new PrismaClient
 const prisma = new PrismaClient();
 
 // Connect to the database
-
 
 // Export the instantiated PrismaClient
 module.exports = prisma;
