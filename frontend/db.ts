88d198f48bde4a7287c75b4e5ca62b0cfaf26d70
nuh <noahbarkatwork@gmail.com>
2024-09-25 19:21:34 -0500

    prisma db troubleshooting

--- frontend/db.ts
+++ frontend/db.ts
@@ -1,11 +1,34 @@
+// import { PrismaClient } from "@prisma/client";
+
+// declare global {
+//   var prisma: PrismaClient | undefined;
+// }
+// export const db = global.prisma || new PrismaClient();                  
+
+// if (process.env.NODE_ENV !== "production") global.prisma = db;
+
+
+
+
 import { PrismaClient } from "@prisma/client";
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
-export const db = global.prisma || new PrismaClient();                  
 
-if (process.env.NODE_ENV !== "production") global.prisma = db;
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    log: ["query", "info", "warn", "error"],
+  });
+};
 
+const prisma = global.prisma || prismaClientSingleton();
 
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
 
+prisma
+  .$connect()
+  .then(() => console.log("Connected to the database"))
+  .catch((error) => console.error("Failed to connect to the database:", error));
+
+export default prisma;
