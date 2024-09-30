3a21f234a77a925f932a14ec00710a520a1fbc56
nuh <noahbarkatwork@gmail.com>
2024-09-29 19:46:03 -0500

    load testing

--- backend/middleware/authSession.js
+++ backend/middleware/authSession.js
@@ -1,33 +1,25 @@
-
-
-// const authOptions = require('./authOptions'); 
-
-
-
+// const authOptions = require('./authOptions');
 
 // Middleware to fetch the session using getSession
 async function authSession(req, res, next) {
-
   try {
-            // Get the token from the request header
-            const sessionData = req.body.session;
-            const userData = req.body.values || {};
-        
+    // Get the token from the request header
+    const sessionData = req.body.session;
+    const userData = req.body.values || {};
 
-            console.log(`Session data received:`, sessionData);
-            
-            // if (!sessionData || !sessionData.user || !sessionData.user.email) {
-            //   return res.status(401).send({ error: 'Invalid session data' });
-            // }
-            
-            req.session = sessionData;
-            req.user = userData;
-            next();
-        } catch (error) {
-            console.error('Error in authSession:', error);
-            res.status(401).send({ error: 'Please authenticate' });
-        }
+    console.log(`Session data received:`, sessionData);
 
+    // if (!sessionData || !sessionData.user || !sessionData.user.email) {
+    //   return res.status(401).send({ error: 'Invalid session data' });
+    // }
+
+    req.session = sessionData;
+    req.user = userData;
+    next();
+  } catch (error) {
+    console.error("Error in authSession:", error);
+    res.status(401).send({ error: "Please authenticate" });
+  }
 }
 
 module.exports = authSession;
--- backend/routes/Jobs.js
+++ backend/routes/Jobs.js
@@ -1,48 +1,50 @@
-const express = require('express');
-const authSession = require('../middleware/authSession');
-const { getUsersJobs, createJob, deleteJob, updateJob } = require('../controllers/jobControllers'); 
-const rateLimit = require('express-rate-limit');
+const express = require("express");
+const authSession = require("../middleware/authSession");
+const {
+  getUsersJobs,
+  createJob,
+  deleteJob,
+  updateJob,
+} = require("../controllers/jobControllers");
+// const rateLimit = require("express-rate-limit");
 
 const router = express.Router();
-
 
 // middle ware
 
 router.use(authSession);
 
-const initialLoadLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 20, // More lenient limit for initial page loads (1 per minute on average)
-    handler: (req, res) => {
-        res.status(429).json({
-            error: 'Too Many Requests',
-            message: 'You have exceeded the rate limit. Please try again later.'
-        });
-    },
-    
-  });
+// const initialLoadLimiter = rateLimit({
+//   windowMs: 15 * 60 * 1000, // 15 minutes
+//   max: 20, // More lenient limit for initial page loads (1 per minute on average)
+//   handler: (req, res) => {
+//     res.status(429).json({
+//       error: "Too Many Requests",
+//       message: "You have exceeded the rate limit. Please try again later.",
+//     });
+//   },
+// });
 
-const limiter = rateLimit({
-    windowMs: 10 * 60 * 1000, // 10 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
-    handler: (req, res) => {
-        res.status(429).json({
-            error: 'Too Many Requests',
-            message: 'You have exceeded the rate limit. Please try again later.'
-        });
-    },
-}); 
-  
-router.use(initialLoadLimiter);
-router.use(limiter);
+// const limiter = rateLimit({
+//   windowMs: 10 * 60 * 1000, // 10 minutes
+//   max: 100, // limit each IP to 100 requests per windowMs
+//   handler: (req, res) => {
+//     res.status(429).json({
+//       error: "Too Many Requests",
+//       message: "You have exceeded the rate limit. Please try again later.",
+//     });
+//   },
+// });
 
-router.post('/', getUsersJobs );
+// router.use(initialLoadLimiter);
+// router.use(limiter);
 
+router.post("/", getUsersJobs);
 
-router.post('/create', createJob);
+router.post("/create", createJob);
 
-router.delete('/delete/:id', deleteJob);
+router.delete("/delete/:id", deleteJob);
 
-router.patch('/update/:id', updateJob);
+router.patch("/update/:id", updateJob);
 
 module.exports = router;
